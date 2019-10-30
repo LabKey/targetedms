69dd11ed5586d70232d1f6a4d3c1f15a7a0a3d82
labkey-jeckels <jeckels@labkey.com>
2019-10-30 10:10:35 -0700

    More comments

--- test/src/org/labkey/test/tests/targetedms/TargetedMSListTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSListTest.java
@@ -92,7 +92,7 @@ public class TargetedMSListTest extends TargetedMSTest
     /** @return the list of queries in the targetedmslist schema */
     private List<String> validateSampleInfo(int replicateRowCount, Set<String> expectedReplicateLookupValues, int sampleUnionRowCount, int listQueryCount) throws IOException, CommandException
     {
-        // Check that the targetedms.replicate table has the expected contents
+        // Check that the targetedms.replicate table has the expected rows and points to the right lookup values
         SelectRowsCommand replicateRowsCommand = new SelectRowsCommand("targetedms", "replicate");
         replicateRowsCommand.setRequiredVersion(9.1);
         SelectRowsResponse replicateRowsResponse = replicateRowsCommand.execute(createDefaultConnection(false), getCurrentContainerPath());
@@ -101,9 +101,11 @@ public class TargetedMSListTest extends TargetedMSTest
         replicateRowsResponse.getRowset().forEach((r) -> replicateSampleLookupValues.add((String)r.getDisplayValue("Sample")));
         assertEquals("Wrong sample names", expectedReplicateLookupValues, replicateSampleLookupValues);
 
+        // Check the contents of the unioned "Samples" lookup query
         SelectRowsCommand sampleRowsUnionedCommand = new SelectRowsCommand("targetedmslists", "All_Samples");
         assertEquals("Wrong number of unioned sample list rows", sampleUnionRowCount, sampleRowsUnionedCommand.execute(createDefaultConnection(false), getCurrentContainerPath()).getRowCount().intValue());
 
+        // Check the schema to make sure it's exposing the expected single-list and unioned-list queries
         GetQueriesCommand command = new GetQueriesCommand("targetedmslists");
         GetQueriesResponse queriesResponse = command.execute(createDefaultConnection(false), getCurrentContainerPath());
         assertEquals("Wrong number of queries", listQueryCount, queriesResponse.getQueryNames().size());
