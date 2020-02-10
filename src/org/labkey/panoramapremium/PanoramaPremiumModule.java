255aca8dcb586563aa5d41783bbebdc33a6ed0fd
Adam Rauch <adam@labkey.com>
2020-02-09 22:41:59 -0700

    Migrate to getSchemaVersion(). Remove ConsolidateScripts. (#25)

--- src/org/labkey/panoramapremium/PanoramaPremiumModule.java
+++ src/org/labkey/panoramapremium/PanoramaPremiumModule.java
@@ -6,6 +6,7 @@
 package org.labkey.panoramapremium;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
 import org.labkey.api.module.DefaultModule;
@@ -46,7 +47,7 @@ public class PanoramaPremiumModule extends DefaultModule
     }
 
     @Override
-    public double getVersion()
+    public @Nullable Double getSchemaVersion()
     {
         return 20.000;
     }
