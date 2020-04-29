cca8dfdff84aa236b4e53d65f158a2a52e1124e4
labkey-jeckels <Josh Eckels>
2020-04-29 11:27:37 -0700

    One more adjustment to fix the default guide set's date range
    Eliminate the need for one of the 61-second waits in the tests by pinging both folders at the same time

--- resources/queries/targetedms/DefaultGuideSet.sql
+++ resources/queries/targetedms/DefaultGuideSet.sql
@@ -3,5 +3,7 @@ SELECT
        0 AS RowId,
        COALESCE(MIN(AcquiredTime), curdate()) AS TrainingStart,
        COALESCE(MAX(AcquiredTime), curdate()) AS TrainingEnd,
-       CAST(NULL AS TIMESTAMP) AS ReferenceEnd
+       -- ReferenceEnd should be null if there's no subsequent guide set, or the value of the start of the next guide
+       -- set if there is
+       (SELECT MIN(TrainingStart) FROM GuideSet) AS ReferenceEnd
 FROM targetedms.SampleFile WHERE AcquiredTime < COALESCE((SELECT MIN(TrainingStart) FROM GuideSet), curdate())
