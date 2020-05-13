22b8ccf7c93caefa1172d7fd50675743b1ccddbb
Josh Eckels <jeckels@labkey.com>
2020-05-12 18:33:57 -0700

    Update to match APIs for QC perf improvements (#30)
    Also, small improvements to spacing on email subscription page

--- src/org/labkey/panoramapremium/QCNotificationSender.java
+++ src/org/labkey/panoramapremium/QCNotificationSender.java
@@ -12,7 +12,6 @@ import org.labkey.api.settings.LookAndFeelProperties;
 import org.labkey.api.targetedms.ITargetedMSRun;
 import org.labkey.api.targetedms.SkylineDocumentImportListener;
 import org.labkey.api.targetedms.TargetedMSService;
-import org.labkey.api.targetedms.model.LJOutlier;
 import org.labkey.api.targetedms.model.SampleFileInfo;
 import org.labkey.api.util.DateUtil;
 import org.labkey.api.util.ExceptionUtil;
@@ -21,11 +20,9 @@ import org.labkey.api.util.PageFlowUtil;
 import org.labkey.panoramapremium.model.UserSubscription;
 
 import javax.mail.Message;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class QCNotificationSender implements SkylineDocumentImportListener
 {
@@ -45,34 +42,29 @@ public class QCNotificationSender implements SkylineDocumentImportListener
         {
             //check if the user is subscribed to receive qc notifications
             List<UserSubscription> userSubscriptions = getUserSubscription(container, user);
-            Map<String, SampleFileInfo> sampleFiles = TargetedMSService.get().getSampleFiles(container, user, QC_SAMPLE_FILE_LIMIT);
 
-            if (!userSubscriptions.isEmpty() && !sampleFiles.isEmpty())
+            if (!userSubscriptions.isEmpty())
             {
+                List<SampleFileInfo> sampleFiles = TargetedMSService.get().getSampleFiles(container, user, QC_SAMPLE_FILE_LIMIT);
+
                 //sorting the map to show latest run first
-                Map<String, SampleFileInfo> sortedSampleFiles = sampleFiles.entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.comparing(SampleFileInfo::getAcquiredTime).reversed()))
-                        .collect(Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (oldVal, newVal) -> oldVal, LinkedHashMap::new));
+                sampleFiles.sort(Comparator.comparing(SampleFileInfo::getAcquiredTime).reversed());
 
                 userSubscriptions.forEach(userSubscription -> {
                     int totalOutliers = 0;
                     int totalOutlierSubscribed = userSubscription.getOutliers();
                     int sampleCount = 0;
 
-                    Map<String, SampleFileInfo> outliersMap = new LinkedHashMap<>();
+                    List<SampleFileInfo> samplesToEmail = new ArrayList<>();
 
-                    for (Map.Entry<String, SampleFileInfo> entry : sortedSampleFiles.entrySet())
+                    for (SampleFileInfo sampleFileInfoDetails : sampleFiles)
                     {
                         sampleCount++;
-                        SampleFileInfo sampleFileInfoDetails = entry.getValue();
 
                         if (sampleFileInfoDetails.getLeveyJennings() > 0 || sampleFileInfoDetails.getmR() > 0 || sampleFileInfoDetails.getCUSUMm() > 0 || sampleFileInfoDetails.getCUSUMv() > 0)
                         {
                             totalOutliers += sampleFileInfoDetails.getLeveyJennings() + sampleFileInfoDetails.getmR() + sampleFileInfoDetails.getCUSUMm() + sampleFileInfoDetails.getCUSUMv();
-                            outliersMap.put(entry.getKey(), entry.getValue());
+                            samplesToEmail.add(sampleFileInfoDetails);
                         }
 
                         if(sampleCount == userSubscription.getSamples())
@@ -84,7 +76,7 @@ public class QCNotificationSender implements SkylineDocumentImportListener
                     //send email
                     if (totalOutliers >= totalOutlierSubscribed)
                     {
-                        sendQCNotification(container, userSubscription, outliersMap);
+                        sendQCNotification(container, userSubscription, samplesToEmail);
                     }
                 });
             }
@@ -117,13 +109,13 @@ public class QCNotificationSender implements SkylineDocumentImportListener
         }
     }
 
-    private void sendQCNotification(Container container, UserSubscription userSubscription, Map<String, SampleFileInfo> outliersMap)
+    private void sendQCNotification(Container container, UserSubscription userSubscription, List<SampleFileInfo> samplesToEmail)
     {
         try
         {
             User recipient = UserManager.getUser(userSubscription.getUserId());
             ValidEmail ve = new ValidEmail(recipient.getEmail());
-            EmailSubjectAndBody emailSubjectAndBody = buildMessage(container, recipient, outliersMap);
+            EmailSubjectAndBody emailSubjectAndBody = buildMessage(container, recipient, samplesToEmail);
 
             MailHelper.MultipartMessage msg = MailHelper.createMultipartMessage();
             msg.setFrom(LookAndFeelProperties.getInstance(container).getSystemEmailAddress());
@@ -138,7 +130,7 @@ public class QCNotificationSender implements SkylineDocumentImportListener
         }
     }
 
-    private EmailSubjectAndBody buildMessage(Container container, User user, Map<String, SampleFileInfo> outliersMap)
+    private EmailSubjectAndBody buildMessage(Container container, User user, List<SampleFileInfo> samplesToEmail)
     {
         EmailSubjectAndBody emailSubjectAndBody = new EmailSubjectAndBody();
         StringBuilder html = new StringBuilder();
@@ -148,10 +140,9 @@ public class QCNotificationSender implements SkylineDocumentImportListener
         html.append("<p><a href=\"").append(container.getStartURL(user)).append("\">View in Panorama</a></p>");
 
         //create separate table for each sample file having outliers
-        for (Map.Entry<String, SampleFileInfo> entry : outliersMap.entrySet())
+        for (SampleFileInfo sampleFileInfoDetails : samplesToEmail)
         {
-            SampleFileInfo sampleFileInfoDetails = entry.getValue();
-            html.append("<h4>").append(PageFlowUtil.filter(entry.getKey())).append(" - ").append(sampleFileInfoDetails.getLeveyJennings() + sampleFileInfoDetails.getmR() + sampleFileInfoDetails.getCUSUMm() + sampleFileInfoDetails.getCUSUMv()).append(" total outliers</h4>");
+            html.append("<h4>").append(PageFlowUtil.filter(sampleFileInfoDetails.getSampleFile())).append(" - ").append(sampleFileInfoDetails.getLeveyJennings() + sampleFileInfoDetails.getmR() + sampleFileInfoDetails.getCUSUMm() + sampleFileInfoDetails.getCUSUMv()).append(" total outliers</h4>");
             html.append("<p>Acquired ").append(DateUtil.formatDateTime(container, sampleFileInfoDetails.getAcquiredTime())).append("</p>");
             html.append("<table style=\"border: 1px solid #d3d3d3;\"><thead><tr><td style=\"border: 1px solid #d3d3d3;\"></td><td  style=\"border: 1px solid #d3d3d3;\" colspan=\"7\" align=\"center\">Outliers</td></tr>")
                     .append("<tr>")
@@ -171,21 +162,18 @@ public class QCNotificationSender implements SkylineDocumentImportListener
                     .append("</tr>")
                     .append("</thead><tbody>");
 
-            sampleFileInfoDetails.getItems().sort(Comparator.comparing(LJOutlier::getMetricLabel));
-
-            sampleFileInfoDetails.getItems().forEach(outlier -> {
+            sampleFileInfoDetails.getByMetric().forEach((key, outlier) -> {
                 var outlierTotal = outlier.getLeveyJennings() + outlier.getmR() + outlier.getCUSUMmN() + outlier.getCUSUMmP() + outlier.getCUSUMvN() + outlier.getCUSUMvP();
                 html.append("<tr>")
-                        .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(PageFlowUtil.filter(outlier.getMetricLabel())).append("</td>")
+                        .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(PageFlowUtil.filter(key)).append("</td>")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getLeveyJennings() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getLeveyJennings())).append("</td>").append(outlier.getLeveyJennings() > 0 ? "</b>" : "")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getmR() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getmR())).append("</td>").append(outlier.getmR() > 0 ? "</b>" : "")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getCUSUMmN() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getCUSUMmN())).append("</td>").append(outlier.getCUSUMmN() > 0 ? "</b>" : "")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getCUSUMmP() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getCUSUMmP())).append("</td>").append(outlier.getCUSUMmP() > 0 ? "</b>" : "")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getCUSUMvN() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getCUSUMvN())).append("</td>").append(outlier.getCUSUMvN() > 0 ? "</b>" : "")
                         .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append(outlier.getCUSUMvP() > 0 ? "<b>" : "").append(PageFlowUtil.filter(outlier.getCUSUMvP())).append("</td>").append(outlier.getCUSUMvP() > 0 ? "</b>" : "")
-                        .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append( "<b>").append(PageFlowUtil.filter(outlierTotal)).append( "</b>").append("</td>");
+                        .append("<td style=\"border: 1px solid #d3d3d3; text-align: right;\">").append("<b>").append(PageFlowUtil.filter(outlierTotal)).append("</b>").append("</td>");
                 html.append("</tr>");
-
             });
 
             // Remember the total for the most recent import
