66c72cc2d89499146578374ae77143f4b746a245
labkey-sweta <swetaj@labkey.com>
2021-02-01 23:08:37 -0800

    Removing the try-catch block and changing the timeout.

--- test/src/org/labkey/test/tests/targetedms/TargetedMSChromatogramOptimizationTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSChromatogramOptimizationTest.java
@@ -6,21 +6,18 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
-import org.labkey.test.WebTestHelper;
 import org.labkey.test.categories.DailyB;
-import org.labkey.test.categories.MS2;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.targetedms.ConnectionSource;
 
 import java.io.File;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Category({DailyB.class})
-@BaseWebDriverTest.ClassTimeout(minutes = 6)
+@BaseWebDriverTest.ClassTimeout(minutes = 3)
 public class TargetedMSChromatogramOptimizationTest extends TargetedMSTest
 {
     private static final String SKY_FILE = "SmallMoleculeLibrary3.sky.zip";
@@ -51,9 +48,6 @@ public class TargetedMSChromatogramOptimizationTest extends TargetedMSTest
         DataRegionTable table = viewQueryData("targetedms", "TransitionOptimization");
         table.rowSelector().showAll();
 
-        checker().verifyEquals("Invalid number of rows in transition optimization", table.getDataRowCount(),
-                sizeOfTable(downloadedClibFile, "MoleculeTransitionOptimization"));
-
         log("Verifying the new tables added");
         checker().verifyTrue("TransitionOptimization table is not present in the SQLITE file",
                 tableExists(downloadedClibFile, "TransitionOptimization"));
@@ -66,61 +60,43 @@ public class TargetedMSChromatogramOptimizationTest extends TargetedMSTest
         checker().verifyTrue("Sample File does not have DpPredictorId",
                 columnExists(downloadedClibFile, "SampleFile", "DpPredictorId"));
 
+        checker().verifyEquals("Invalid number of rows in transition optimization", table.getDataRowCount(),
+                sizeOfTable(downloadedClibFile, "MoleculeTransitionOptimization"));
+
     }
 
-    private int sizeOfTable(File clibFile, String name)
-
+    private int sizeOfTable(File clibFile, String name) throws SQLException
     {
         int cnt = 0;
         @SuppressWarnings("SqlResolve")
         String sql = "SELECT * FROM " + name;
-        try (
-                Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath()))
-        {
-            ResultSet rs = conn.createStatement().executeQuery(sql);
-            while (rs.next())
-                cnt++;
-        }
-        catch (SQLException e)
-        {
-            throw new RuntimeException(e);
-        }
+        Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath());
+        ResultSet rs = conn.createStatement().executeQuery(sql);
+        while (rs.next())
+            cnt++;
         return cnt;
     }
 
-    private boolean tableExists(File clibFile, String tableName)
+    private boolean tableExists(File clibFile, String tableName) throws SQLException
     {
-        try (
-                Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath()))
-        {
-            DatabaseMetaData md = conn.getMetaData();
-            ResultSet rs = md.getTables(null, null, tableName, null);
-            if (rs.next())
-                return true; //Table exists
-            else
-                return false;
-        }
-        catch (SQLException e)
-        {
-            throw new RuntimeException(e);
-        }
+        Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath());
+        DatabaseMetaData md = conn.getMetaData();
+        ResultSet rs = md.getTables(null, null, tableName, null);
+        if (rs.next())
+            return true; //Table exists
+        else
+            return false;
     }
 
-    private boolean columnExists(File clibFile, String tableName, String columnName)
+    private boolean columnExists(File clibFile, String tableName, String columnName) throws SQLException
     {
-        try (
-                Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath()))
-        {
-            DatabaseMetaData md = conn.getMetaData();
-            ResultSet rs = md.getColumns(null, null, tableName, columnName);
-            if (rs.next())
-                return true; //Table exists
-            else
-                return false;
-        }
-        catch (SQLException e)
-        {
-            throw new RuntimeException(e);
-        }
+        Connection conn = ConnectionSource.getConnection(clibFile.getAbsolutePath());
+        DatabaseMetaData md = conn.getMetaData();
+        ResultSet rs = md.getColumns(null, null, tableName, columnName);
+        if (rs.next())
+            return true; //Table exists
+        else
+            return false;
+
     }
 }
