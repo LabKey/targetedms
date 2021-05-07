a1818807cdcc2ce311e838502d2638630c1e11a8
labkey-jeckels <Josh Eckels>
2021-05-07 10:09:24 -0700

    Fix typo in xpath

--- test/src/org/labkey/test/components/targetedms/CalibrationCurveWebpart.java
+++ test/src/org/labkey/test/components/targetedms/CalibrationCurveWebpart.java
@@ -99,7 +99,7 @@ public class CalibrationCurveWebpart extends BodyWebPart<CalibrationCurveWebpart
     protected class ElementCache extends BodyWebPart.ElementCache
     {
         private final WebElement exportToPng = Locator.id("targetedmsCalibrationCurvefa-image-o").findWhenNeeded(this);
-        private final WebElement exportToPdf = Locator.id("targetedmsCalibrationCurvefa--pdf-o").findWhenNeeded(this);
+        private final WebElement exportToPdf = Locator.id("targetedmsCalibrationCurvefa-pdf-o").findWhenNeeded(this);
         private final WebElement svg = Locator.css("svg").findWhenNeeded(this);
         private List<WebElement> findPoints()
         {
