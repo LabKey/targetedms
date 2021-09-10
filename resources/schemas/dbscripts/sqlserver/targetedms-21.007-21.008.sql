fce8160b277006a4302630a09b6e4770381173e5
labkey-jeckels <Josh Eckels>
2021-09-10 10:44:34 -0700

    Slight wording improvements

--- resources/schemas/dbscripts/sqlserver/targetedms-21.007-21.008.sql
+++ resources/schemas/dbscripts/sqlserver/targetedms-21.007-21.008.sql
@@ -7,13 +7,13 @@ declare @rootIdentity ENTITYID;
 select @rootIdentity = [EntityId] FROM [core].[Containers] WHERE Parent is null
 
 INSERT INTO targetedms.QCMetricConfiguration (Container, Name, Series1Label, Series1SchemaName, Series1QueryName, PrecursorScoped, EnabledQueryName, EnabledSchemaName) VALUES
-    (@rootIdentity, 'iRT Slope', 'Slope', 'targetedms', 'QCRunMetric_iRTSlope', 0, 'QCRunMetricEnabled_iRTSlope', 'targetedms');
+    (@rootIdentity, 'iRT Slope', 'iRT Slope', 'targetedms', 'QCRunMetric_iRTSlope', 0, 'QCRunMetricEnabled_iRTSlope', 'targetedms');
 
 INSERT INTO targetedms.QCMetricConfiguration (Container, Name, Series1Label, Series1SchemaName, Series1QueryName, PrecursorScoped, EnabledQueryName, EnabledSchemaName) VALUES
-    (@rootIdentity, 'iRT Intercept', 'Intercept', 'targetedms', 'QCRunMetric_iRTIntercept', 0, 'QCRunMetricEnabled_iRTIntercept', 'targetedms');
+    (@rootIdentity, 'iRT Intercept', 'iRT Intercept', 'targetedms', 'QCRunMetric_iRTIntercept', 0, 'QCRunMetricEnabled_iRTIntercept', 'targetedms');
 
 INSERT INTO targetedms.QCMetricConfiguration (Container, Name, Series1Label, Series1SchemaName, Series1QueryName, PrecursorScoped, EnabledQueryName, EnabledSchemaName) VALUES
-    (@rootIdentity, 'iRT Correlation', 'Correlation', 'targetedms', 'QCRunMetric_iRTCorrelation', 0, 'QCRunMetricEnabled_iRTCorrelation', 'targetedms');
+    (@rootIdentity, 'iRT Correlation', 'iRT Correlation', 'targetedms', 'QCRunMetric_iRTCorrelation', 0, 'QCRunMetricEnabled_iRTCorrelation', 'targetedms');
 
 UPDATE targetedms.QCMetricConfiguration SET EnabledQueryName = 'QCRunMetricEnabled_ticArea', EnabledSchemaName = 'targetedms' WHERE Name = 'TIC Area';
 UPDATE targetedms.QCMetricConfiguration SET EnabledQueryName = 'QCMetricEnabled_massAccuracy', EnabledSchemaName = 'targetedms' WHERE Name = 'Mass Accuracy';
