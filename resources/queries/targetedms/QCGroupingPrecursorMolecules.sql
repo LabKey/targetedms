d00f4e0d0195653a0968214222097fed1547edeb
Binal Patel <binalp@labkey.com>
2022-02-18 06:33:19 -0800

    Updates for precursor identifier. Add molecule precursor metadata.

--- resources/queries/targetedms/QCGroupingPrecursorMolecules.sql
+++ resources/queries/targetedms/QCGroupingPrecursorMolecules.sql
@@ -1,27 +1,40 @@
 -- This query for Molecules displays under QC Summary menu 'Include or Exclude Peptides/Molecules'
 
 SELECT
-mol.peptideGroupId.Label,
--- mol.PeptideGroupId.RunId.File,
-molPrec.IonFormula,
-molPrec.customIonName,
-molPrec.charge,
-molPrec.mz,
-molPrec.massAverage,
-molPrec.massMonoisotopic
+    *,
+    (CASE
+         WHEN molPrec.precursorIdentifier IN (SELECT precursorIdentifier FROM targetedms.ExcludedPrecursors) THEN 'Excluded'
+         ELSE 'Included'
+        END) AS markedAs,
 FROM
-    targetedms.Molecule mol
-LEFT JOIN
-    targetedms.MoleculePrecursor molPrec
-ON
-    mol.IonFormula = molPrec.moleculeId.ionFormula
+     (SELECT
+        (molPrec.customIonName || ', ' ||
+         molPrec.IonFormula || ', ' ||
+         cast(molPrec.massMonoisotopic AS VARCHAR) || ', ' ||
+         cast(molPrec.massAverage AS VARCHAR) ||', '||
+         cast(molPrec.charge AS VARCHAR) ||', '||
+         cast(molPrec.mz AS VARCHAR)) AS precursorIdentifier, -- "key field" required for 'requireSelection' to work
+        mol.peptideGroupId.Label,
+        -- mol.PeptideGroupId.RunId.File,
+        molPrec.customIonName,
+        molPrec.IonFormula,
+        molPrec.charge,
+        molPrec.mz,
+        molPrec.massMonoisotopic,
+        molPrec.massAverage
+        FROM
+            targetedms.Molecule mol
+        LEFT JOIN
+            targetedms.MoleculePrecursor molPrec
+        ON
+            mol.IonFormula = molPrec.moleculeId.ionFormula
 
-GROUP BY
-    mol.peptideGroupId.Label,
--- mol.PeptideGroupId.RunId.File,
-    molPrec.IonFormula,
-    molPrec.customIonName,
-    molPrec.charge,
-    molPrec.mz,
-    molPrec.massAverage,
-    molPrec.massMonoisotopic
+        GROUP BY
+            mol.peptideGroupId.Label,
+        -- mol.PeptideGroupId.RunId.File,
+            molPrec.IonFormula,
+            molPrec.customIonName,
+            molPrec.charge,
+            molPrec.mz,
+            molPrec.massAverage,
+            molPrec.massMonoisotopic) molPrec
--- resources/queries/targetedms/QCGroupingPrecursorPeptides.sql
+++ resources/queries/targetedms/QCGroupingPrecursorPeptides.sql
@@ -5,10 +5,10 @@ SELECT
 (CASE
     WHEN precPep.precursorIdentifier IN (SELECT precursorIdentifier FROM targetedms.ExcludedPrecursors) THEN 'Excluded'
     ELSE 'Included'
-END) AS markedAs,
+END) AS markedAs
 FROM
      (SELECT
-        (pep.PeptideModifiedSequence || '-' || cast(prec.mz AS VARCHAR)) AS precursorIdentifier, -- "key field" required for requireSelection to work
+        (pep.PeptideModifiedSequence || ', ' || cast(prec.charge AS VARCHAR) || ', ' || cast(prec.mz AS VARCHAR)) AS precursorIdentifier, -- "key field" required for 'requireSelection' to work
         pep.PeptideModifiedSequence AS peptideSequence,
         -- pep.PeptideGroupId.RunId.File,
         pep.PeptideGroupId.Label,
