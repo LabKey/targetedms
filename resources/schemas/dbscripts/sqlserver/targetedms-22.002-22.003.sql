9cc9681e263c1dafb39574509692974abb3fcdad
labkey-martyp <martyp@labkey.com>
2022-03-13 14:26:26 -0700

    Too many view creations

--- resources/schemas/dbscripts/sqlserver/targetedms-22.002-22.003.sql
+++ resources/schemas/dbscripts/sqlserver/targetedms-22.002-22.003.sql
@@ -40,38 +40,3 @@ GO
 ALTER TABLE targetedms.AuditLogEntry DROP COLUMN VersionId;
 GO
 
-CREATE VIEW targetedms.AuditLog AS
-
-WITH logTree as (
-    SELECT entryId
-         , entryHash
-         , parentEntryHash
-    FROM targetedms.AuditLogEntry e
-    WHERE e.parentEntryHash = '(null)'
-
-    UNION ALL
-
-    SELECT nxt.entryId
-         , nxt.entryHash
-         , nxt.parentEntryHash
-    FROM targetedms.AuditLogEntry nxt
-             JOIN logTree prev
-                  ON prev.parentEntryHash = nxt.entryHash
-)
-SELECT t.entryId
-     , e.documentguid
-     , e.entryHash
-     , ra.VersionId AS RunId
-     , e.createtimestamp
-     , e.timezoneoffset
-     , e.username
-     , e.formatversion
-     , e.parentEntryHash
-     , e.reason
-     , e.extrainfo
-
-FROM logTree t
-         INNER JOIN targetedms.AuditLogEntry e ON (t.EntryId = e.EntryId)
-         LEFT JOIN targetedms.RunAuditLogEntry ra ON t.entryId = ra.AuditLogEntryId
-WHERE ra.VersionId IS NOT NULL
-    );
