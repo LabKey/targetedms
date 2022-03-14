96717a16fb158c341c03daab925b027ef54c0333
Binal Patel <binalp@labkey.com>
2022-03-13 22:36:38 -0700

    Update comments

--- src/org/labkey/targetedms/folderImport/QCFolderWriterFactory.java
+++ src/org/labkey/targetedms/folderImport/QCFolderWriterFactory.java
@@ -27,24 +27,23 @@ public class QCFolderWriterFactory implements FolderWriterFactory
         @Override
         public void write(Container object, ImportContext<FolderDocument.Folder> ctx, VirtualFile root) throws Exception
         {
-            //TODO: write QC settings into QCFolderImporter.QC_FOLDER_DIR:
-            //New custom metrics and trace metrics
-            //Metric configurations and settings
-            //Guide sets
-            //Excluded samples and the metric(s) the samples are being excluded from
-            //Peptide/molecule groups
-            //Annotations - QC Annotation, QC Annotation Type, Replicate Annotation
-            //Plot settings from default view - Plot metric, Date Range, Plot size, QC Plot type, Y-Axis Scale
-            //Replicate Annotation filter, if any
+            //TODO: Get these QC settings from appropriate tables and write it to a .tsv or .properties (if applicable) into QCFolderImporter.QC_FOLDER_DIR:
+            //1. New custom metrics and trace metrics
+            //2. Metric configurations and settings
+            //3. Guide sets
+            //4. Excluded samples and the metric(s) the samples are being excluded from Peptide/molecule groups
+            //5. Annotations - QC Annotation, QC Annotation Type, Replicate Annotation
+            //6. Plot settings from default view - Plot metric, Date Range, Plot size, QC Plot type, Y-Axis Scale
+            //7. Replicate Annotation filter, if any
 
-            //plot display options:
-            //Group X-Axis Values by Date - checkbox value
-            //Show All Series in a Single Plot - checkbox value
-            //Show Excluded Points - checkbox value
-            //Show Reference Guide Set - checkbox value
-            //Show Excluded Precursors– checkbox value
+            //8. plot display options:
+            //-Group X-Axis Values by Date - checkbox value
+            //-Show All Series in a Single Plot - checkbox value
+            //-Show Excluded Points - checkbox value
+            //-Show Reference Guide Set - checkbox value
+            //-Show Excluded Precursors– checkbox value
         }
 
-        // TODO: override other necessary methods, if needed
+        // TODO: override other methods, if needed
     }
 }
