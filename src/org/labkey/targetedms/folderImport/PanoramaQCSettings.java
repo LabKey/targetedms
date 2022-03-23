4b43bf8ac7326e0c8cd26c8ca887908a7e6bf3a6
Binal Patel <binalp@labkey.com>
2022-03-22 21:07:28 -0700

    Implement tsv import (wip)

--- src/org/labkey/targetedms/folderImport/PanoramaQCSettings.java
+++ src/org/labkey/targetedms/folderImport/PanoramaQCSettings.java
@@ -3,6 +3,7 @@ package org.labkey.targetedms.folderImport;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.admin.FolderImportContext;
+import org.labkey.api.audit.TransactionAuditProvider;
 import org.labkey.api.data.ColumnHeaderType;
 import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.CompareType;
@@ -14,8 +15,14 @@ import org.labkey.api.data.SimpleFilter;
 import org.labkey.api.data.TSVGridWriter;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.data.TableSelector;
+import org.labkey.api.gwt.client.AuditBehaviorType;
+import org.labkey.api.query.AbstractQueryImportAction;
+import org.labkey.api.query.BatchValidationException;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.QueryService;
+import org.labkey.api.query.QueryUpdateService;
+import org.labkey.api.reader.DataLoader;
+import org.labkey.api.reader.TabLoader;
 import org.labkey.api.security.User;
 import org.labkey.api.util.logging.LogHelper;
 import org.labkey.api.writer.VirtualFile;
@@ -30,6 +37,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.labkey.api.query.AbstractQueryUpdateService.createTransactionAuditEvent;
 
 public enum PanoramaQCSettings
 {
@@ -55,29 +64,19 @@ public enum PanoramaQCSettings
 
     public static @Nullable PanoramaQCSettings getSetting(String fileName)
     {
-        switch (fileName)
-        {
-            case QCFolderConstants.QC_METRIC_CONFIGURATION_FILE_NAME:
-                return METRIC_CONFIG;
-            case QCFolderConstants.QC_ENABLED_METRICS_FILE_NAME:
-                return QC_ENABLED_METRICS;
-            case QCFolderConstants.GUIDE_SET_FILE_NAME:
-                return GUIDE_SET;
-            case QCFolderConstants.QC_METRIC_EXCLUSION_FILE_NAME:
-                return QC_METRIC_EXCLUSION;
-            case QCFolderConstants.PEPTIDE_MOLECULE_PRECURSOR_EXCLUSION_FILE_NAME:
-                return PRECURSOR_EXCLUSION;
-            case QCFolderConstants.QC_ANNOTATION_FILE_NAME:
-                return QC_ANNOTATION;
-            case QCFolderConstants.QC_ANNOTATION_TYPE:
-                return QC_ANNOTATION_TYPE;
-            case QCFolderConstants.REPLICATE_ANNOTATION_FILE_NAME:
-                return REPLICATE_ANNOTATION;
-            case QCFolderConstants.QC_PLOT_SETTINGS_PROPS_FILE_NAME:
-                return QC_PLOT_SETTINGS;
-            default:
-                return null;
-        }
+        return switch (fileName)
+                {
+                    case QCFolderConstants.QC_METRIC_CONFIGURATION_FILE_NAME -> METRIC_CONFIG;
+                    case QCFolderConstants.QC_ENABLED_METRICS_FILE_NAME -> QC_ENABLED_METRICS;
+                    case QCFolderConstants.GUIDE_SET_FILE_NAME -> GUIDE_SET;
+                    case QCFolderConstants.QC_METRIC_EXCLUSION_FILE_NAME -> QC_METRIC_EXCLUSION;
+                    case QCFolderConstants.PEPTIDE_MOLECULE_PRECURSOR_EXCLUSION_FILE_NAME -> PRECURSOR_EXCLUSION;
+                    case QCFolderConstants.QC_ANNOTATION_FILE_NAME -> QC_ANNOTATION;
+                    case QCFolderConstants.QC_ANNOTATION_TYPE -> QC_ANNOTATION_TYPE;
+                    case QCFolderConstants.REPLICATE_ANNOTATION_FILE_NAME -> REPLICATE_ANNOTATION;
+                    case QCFolderConstants.QC_PLOT_SETTINGS_PROPS_FILE_NAME -> QC_PLOT_SETTINGS;
+                    default -> null;
+                };
     }
 
     protected void writeResults(String name, VirtualFile vf, Container container, User user)
@@ -101,10 +100,11 @@ public enum PanoramaQCSettings
         TableInfo ti = schema.getTable(_tableName, cf);
 
         if (METRIC_CONFIG.name().equals(name))
-            filter = SimpleFilter.createContainerFilter(container);
+            filter = SimpleFilter.createContainerFilter(container); //only export the ones that are defined in current container (and not the ones from the root container)
         else if (REPLICATE_ANNOTATION.name().equals(name))
             filter = new SimpleFilter(FieldKey.fromString("Source"), "Skyline", CompareType.NEQ);
 
+        assert ti != null;
         List<ColumnInfo> userEditableCols = ti.getColumns().stream().filter(ci -> ci.isUserEditable()).collect(Collectors.toList());
         final SimpleFilter finalFilter = filter;
         ResultsFactory factory = ()-> QueryService.get().select(ti, userEditableCols, finalFilter, null, null, false);
@@ -149,7 +149,7 @@ public enum PanoramaQCSettings
 
         Map<String, String> plotSettings = PropertyManager.getProperties(user, container, QCFolderConstants.CATEGORY);
 
-        try (PrintWriter out = vf.getPrintWriter(QCFolderConstants.QC_PLOT_SETTINGS_PROPS_FILE_NAME);) {
+        try (PrintWriter out = vf.getPrintWriter(QCFolderConstants.QC_PLOT_SETTINGS_PROPS_FILE_NAME)) {
 
             Properties prop = new Properties();
             for (String name : plotSettings.keySet())
@@ -195,7 +195,31 @@ public enum PanoramaQCSettings
 
     private void importFromTsv(FolderImportContext ctx, VirtualFile panoramaQCDir)
     {
+        TargetedMSSchema schema = new TargetedMSSchema(ctx.getUser(), ctx.getContainer());
+        TableInfo ti = schema.getTable(_tableName);
+        assert ti != null;
+        QueryUpdateService qus = ti.getUpdateService();
+        try
+        {
+            ctx.getLogger().info("Starting data import from " + _settingsFileName + " into targetedms." + _tableName);
 
+            DataLoader loader = DataLoader.get().createLoader(_settingsFileName, null, panoramaQCDir.getInputStream(_settingsFileName), true, null, TabLoader.TSV_FILE_TYPE);
+
+            AuditBehaviorType behaviorType = ti.getAuditBehavior();
+            TransactionAuditProvider.TransactionAuditEvent auditEvent = null;
+            if (behaviorType != null && behaviorType != AuditBehaviorType.NONE)
+                auditEvent = createTransactionAuditEvent(ctx.getContainer(), QueryService.AuditAction.INSERT);
+
+            BatchValidationException errors = new BatchValidationException();
+            AbstractQueryImportAction.importData(loader, ti, qus, QueryUpdateService.InsertOption.INSERT,
+                    false, false, errors, behaviorType, auditEvent, ctx.getUser(), ctx.getContainer());
+
+            ctx.getLogger().info("Finished importing data from " + _settingsFileName + " into targetedms." + _tableName);
+        }
+        catch (IOException e)
+        {
+            LOG.error("Error importing panorama qc settings from " + _settingsFileName + " into targetedms." + _tableName + ": " + e.getMessage(), e);
+        }
     }
 
     private void importQCProperties(FolderImportContext ctx, VirtualFile panoramaQCDir)
