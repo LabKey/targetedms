4d497d6f143ffb24b7776e7acaba265d2fdfc858
labkey-sweta <swetaj@labkey.com>
2023-06-27 00:05:33 -0700

    Code review changes

--- test/src/org/labkey/test/components/targetedms/UtilizationCalendarWebPart.java
+++ test/src/org/labkey/test/components/targetedms/UtilizationCalendarWebPart.java
@@ -1,12 +1,14 @@
 package org.labkey.test.components.targetedms;
 
 import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
 import org.labkey.test.Locator;
 import org.labkey.test.Locators;
 import org.labkey.test.components.BodyWebPart;
 import org.labkey.test.components.bootstrap.ModalDialog;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UtilizationCalendarWebPart extends BodyWebPart<UtilizationCalendarWebPart.Elements>
 {
@@ -34,7 +36,6 @@ public class UtilizationCalendarWebPart extends BodyWebPart<UtilizationCalendarW
     {
         getWrapper().doAndWaitForElementToRefresh(() -> getWrapper().selectOptionByValue(elementCache().display, value),
                 elementCache().yearTitle, getWrapper().shortWait());
-
         return this;
     }
 
@@ -56,43 +57,38 @@ public class UtilizationCalendarWebPart extends BodyWebPart<UtilizationCalendarW
 
     public String getDisplayedMonth()
     {
-        return elementCache().yearTitle.findWhenNeeded(elementCache().calendar).getText();
+        return elementCache().yearTitle.findWhenNeeded(elementCache().calendar.findWhenNeeded(this)).getText();
     }
 
-    public UtilizationCalendarWebPart markOffline(String startDate, String description)
+    public UtilizationCalendarWebPart markOffline(String startDate, @Nullable String endDate, @Nullable String description)
     {
-        return markOffline(startDate, startDate, description);
-    }
-
-    public UtilizationCalendarWebPart markOffline(String startDate, String endDate, String description)
-    {
-        ModalDialog offlineAnnotationDialog = clickDate(startDate);
-        getWrapper().setFormElement(Locator.name("event-description").findElement(offlineAnnotationDialog.getComponentElement()), description);
-        getWrapper().setFormElement(Locator.name("event-start-date").findElement(offlineAnnotationDialog.getComponentElement()), startDate);
-        getWrapper().setFormElement(Locator.name("event-end-date").findElement(offlineAnnotationDialog.getComponentElement()), endDate);
-        offlineAnnotationDialog.dismiss("Save", 0);
+        ModalDialog modalDialog = clickDate(startDate);
+        getWrapper().setFormElement(Locator.name("event-description").findElement(modalDialog.getComponentElement()), description);
+        getWrapper().setFormElement(Locator.name("event-end-date").findElement(modalDialog.getComponentElement()), endDate);
+        modalDialog.dismiss("Save", 0);
         return this;
     }
 
-    public String getValidationErrorMsg(String startDate, @Nullable String endDate, @Nullable String description)
+    public UtilizationCalendarWebPart markOfflineExpectingError(String startDate, @Nullable String endDate, @Nullable String description, String errorMsg)
     {
         ModalDialog modalDialog = clickDate(startDate);
-        getWrapper().setFormElement(Locator.name("event-start-date").findElement(modalDialog.getComponentElement()), startDate);
         if (description != null)
             getWrapper().setFormElement(Locator.name("event-description").findElement(modalDialog.getComponentElement()), description);
         if (endDate != null)
             getWrapper().setFormElement(Locator.name("event-end-date").findElement(modalDialog.getComponentElement()), endDate);
         modalDialog.dismiss("Save", 0);
-        String errorMsg = Locators.labkeyError.findElement(modalDialog.getComponentElement()).getText();
+        getWrapper().shortWait().until(ExpectedConditions
+                .visibilityOfNestedElementsLocatedBy(modalDialog.getComponentElement(), Locators.labkeyError));
+        Assert.assertEquals("Incorrect validation message", errorMsg, Locators.labkeyError.findElement(modalDialog.getComponentElement()).getText());
         modalDialog.dismiss("Cancel", 0);
-        return errorMsg;
+        return this;
     }
 
     public ModalDialog clickDate(String date)
     {
         String clickDate = date.substring(date.lastIndexOf("-") + 1, date.length());
         getWrapper().log("Clicking " + clickDate + " to update the status");
-        elementCache().dayLoc.withText(clickDate).findElement(elementCache().calendar).click();
+        elementCache().dayLoc.withText(clickDate).findElement(elementCache().calendar.findWhenNeeded(this)).click();
         return new ModalDialog.ModalDialogFinder(getDriver()).withTitle("Instrument Offline Annotation").waitFor();
     }
 
@@ -100,6 +96,25 @@ public class UtilizationCalendarWebPart extends BodyWebPart<UtilizationCalendarW
     {
         String day = date.substring(date.lastIndexOf("-") + 1, date.length());
         return getWrapper().isElementPresent(elementCache().offlineDayLoc.withText(day));
+    }
+
+    public UtilizationCalendarWebPart markOnline(String date)
+    {
+        if (isOffline(date))
+        {
+            ModalDialog modalDialog = clickDate(date);
+            modalDialog.dismiss("Delete", 0);
+        }
+        return this;
+    }
+
+    public String getToolTipText(String date)
+    {
+        String day = date.substring(date.lastIndexOf("-") + 1, date.length());
+        getWrapper().log("Clicking " + day + " to update the status");
+        getWrapper().sleep(500);
+        getWrapper().mouseOver(elementCache().dayLoc.withText(day).findElement(elementCache().calendar.findWhenNeeded(this)));
+        return getWrapper().waitForElement(elementCache().tooltip).getText();
     }
 
     @Override
@@ -112,11 +127,13 @@ public class UtilizationCalendarWebPart extends BodyWebPart<UtilizationCalendarW
     {
         private final WebElement display = Locator.id("monthNumberSelect").findWhenNeeded(this);
         private final WebElement heatMap = Locator.id("heatMapSource").findWhenNeeded(this);
-        private final WebElement calendar = Locator.tagWithId("div", "calendar").findWhenNeeded(this);
+        private final Locator calendar = Locator.tagWithId("div", "calendar");
         private final Locator yearTitle = Locator.tagWithClass("th", "year-title");
         private final Locator dayLoc = Locator.tagWithClassContaining("div", "day-content");
 
         private final Locator offlineDayLoc = Locator.tagWithClassContaining("div", "day-content offline");
+
+        private final Locator tooltip = Locator.tagWithAttribute("div", "role", "tooltip");
 
     }
 }
--- test/src/org/labkey/test/tests/targetedms/TargetedMSUtilizationCalendarTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSUtilizationCalendarTest.java
@@ -9,6 +9,7 @@ import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.components.targetedms.UtilizationCalendarWebPart;
 import org.labkey.test.pages.targetedms.PanoramaDashboard;
 import org.labkey.test.util.DataRegionTable;
+import org.openqa.selenium.NoSuchElementException;
 
 import java.util.Arrays;
 
@@ -53,18 +54,21 @@ public class TargetedMSUtilizationCalendarTest extends TargetedMSTest
         utilizationCalendar.setDisplay("12");
         Assert.assertEquals("Incorrect month displayed", "2012 - 2013", utilizationCalendar.getDisplayedMonth());
 
-        utilizationCalendar.setDisplay("1");
-        Assert.assertEquals("Validation failed for empty description", "Error saving. Missing value for required property: Description",
-                utilizationCalendar.getValidationErrorMsg("2013-08-2", null, ""));
-        Assert.assertEquals("Validation failed for invalid date", "Error saving. Could not convert value '2013-0802' (String) for Timestamp field 'EndDate'",
-                utilizationCalendar.getValidationErrorMsg("2013-08-2", "2013-0802", null));
-        Assert.assertEquals("Validate failed for end date < start date", "Error saving. End date cannot be before the start date",
-                utilizationCalendar.getValidationErrorMsg("2013-08-2", "2013-08-01", null));
+        log("Input data validation checks");
+        utilizationCalendar.setDisplay("1")
+                .markOfflineExpectingError("2013-08-2", null, null, "Error saving. Missing value for required property: Description")
+                .markOfflineExpectingError("2013-08-2", "2013-0802", null, "Error saving. Could not convert value '2013-0802' (String) for Timestamp field 'EndDate'")
+                .markOfflineExpectingError("2013-08-2", "2013-08-01", null, "Error saving. End date cannot be before the start date");
+
 
         log("Marking single day offline");
         String offlineDate = "2013-08-13";
-        utilizationCalendar.markOffline(offlineDate, "Offline for single day");
+        utilizationCalendar.markOffline(offlineDate, offlineDate, "Offline for single day");
         Assert.assertTrue("Date " + offlineDate + " did not update to offline", utilizationCalendar.isOffline(offlineDate));
+
+        log("Marking offline day online");
+        utilizationCalendar.markOnline(offlineDate);
+        Assert.assertFalse("Date " + offlineDate + " did not update to online", utilizationCalendar.isOffline(offlineDate));
 
         log("Marking multiple days offline");
         String startDate = "2013-08-28";
@@ -77,7 +81,7 @@ public class TargetedMSUtilizationCalendarTest extends TargetedMSTest
         log("Verify audit log entries");
         DataRegionTable auditTable = goToAdminConsole().clickAuditLog().selectView("Query update events").getLogTable();
         auditTable.setFilter("ProjectId", "Equals", getProjectName());
-        Assert.assertEquals("Incorrect audit log entries", Arrays.asList("A row was inserted.", "A row was inserted."),
+        Assert.assertEquals("Incorrect audit log entries", Arrays.asList("A row was inserted.", "A row was deleted.", "A row was inserted."),
                 auditTable.getColumnDataAsText("Comment"));
     }
 
@@ -94,10 +98,41 @@ public class TargetedMSUtilizationCalendarTest extends TargetedMSTest
             utilizationCalendar.clickDate("2013-08-01");
             Assert.fail("Readers should not be able to update the status");
         }
-        catch (Exception e)
+        catch (NoSuchElementException e)
         {
-            Assert.assertTrue("Reader cannot update the status", true);
+//            "Reader cannot update the status
         }
         stopImpersonating();
+    }
+
+    @Test
+    public void testHeatMapDataSource()
+    {
+        goToProjectHome();
+        UtilizationCalendarWebPart utilizationCalendar = new PanoramaDashboard(this)
+                .getQcSummaryWebPart()
+                .gotoUtilizationCalendar();
+
+        String startDate = "2013-08-21";
+        Assert.assertEquals("Incorrect Sample count information for " + startDate, "Online\n" +
+                "12 Samples, 0 Median Outliers\n" +
+                "Q_Exactive_08_14_2013_JGB_65 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_66 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_67 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_68 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_69 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_70 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_71 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_72 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_74 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_75 (0 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_76 (1 outliers)\n" +
+                "Q_Exactive_08_14_2013_JGB_78 (1 outliers)", utilizationCalendar.getToolTipText(startDate));
+
+        utilizationCalendar.setHeatMap("Median outliers");
+        startDate = "2013-08-12";
+        Assert.assertEquals("Incorrect Median outliers information for " + startDate, "Online\n" +
+                "1 Sample, 28 Median Outliers\n" +
+                "Q_Exactive_08_09_2013_JGB_38 (28 outliers)", utilizationCalendar.getToolTipText(startDate));
     }
 }
