1bf41fc64bdc0548c0066cb79d4d90d360e8fff4
labkey-jeckels <jeckels@labkey.com>
2023-06-29 09:47:51 -0700

    A little more cleanup

--- src/org/labkey/targetedms/folderImport/QCFolderImporter.java
+++ src/org/labkey/targetedms/folderImport/QCFolderImporter.java
@@ -1,7 +1,5 @@
 package org.labkey.targetedms.folderImport;
 
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.admin.AbstractFolderImportFactory;
 import org.labkey.api.admin.FolderImportContext;
@@ -9,23 +7,14 @@ import org.labkey.api.admin.FolderImporter;
 import org.labkey.api.admin.ImportException;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.pipeline.PipelineJob;
-import org.labkey.api.pipeline.PipelineJobWarning;
 import org.labkey.api.query.BatchValidationException;
-import org.labkey.api.query.DuplicateKeyException;
 import org.labkey.api.query.QueryUpdateService;
-import org.labkey.api.query.QueryUpdateServiceException;
 import org.labkey.api.targetedms.TargetedMSService;
 import org.labkey.api.writer.VirtualFile;
-import org.labkey.folder.xml.ModulePropertyType;
 import org.labkey.targetedms.TargetedMSSchema;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class QCFolderImporter implements FolderImporter
 {
@@ -45,7 +34,7 @@ public class QCFolderImporter implements FolderImporter
     public void process(@Nullable PipelineJob job, FolderImportContext ctx, VirtualFile root) throws Exception
     {
         //if 'PanoramaQC' folder is present in the archive
-        if (!root.listDirs().isEmpty() && root.listDirs().stream().anyMatch((name) -> name.equalsIgnoreCase(QCFolderConstants.QC_FOLDER_DIR)))
+        if (!root.listDirs().isEmpty() && root.listDirs().stream().anyMatch(name -> name.equalsIgnoreCase(QCFolderConstants.QC_FOLDER_DIR)))
         {
             VirtualFile panoramaQCDir = root.getDir(QCFolderConstants.QC_FOLDER_DIR);
             List<String> filesToImport = root.getDir(QCFolderConstants.QC_FOLDER_DIR).list();
@@ -86,8 +75,7 @@ public class QCFolderImporter implements FolderImporter
                                 ctx.getLogger().info("Finished importing " + numRows + " rows from " + qcSetting.getSettingsFileName() + " into targetedms." + qcSetting.getTableName());
                             }
                         }
-                        catch (IOException | DuplicateKeyException | BatchValidationException |
-                               QueryUpdateServiceException | SQLException e)
+                        catch (IOException e)
                         {
                             if (qcSetting.getSettingsFileName().equalsIgnoreCase(QCFolderConstants.QC_PLOT_SETTINGS_PROPS_FILE_NAME))
                             {
@@ -102,12 +90,6 @@ public class QCFolderImporter implements FolderImporter
                 }
             }
         }
-    }
-
-    @Override
-    public @NotNull Collection<PipelineJobWarning> postProcess(FolderImportContext ctx, VirtualFile root) throws Exception
-    {
-        return Collections.emptyList();
     }
 
     public static class Factory extends AbstractFolderImportFactory
