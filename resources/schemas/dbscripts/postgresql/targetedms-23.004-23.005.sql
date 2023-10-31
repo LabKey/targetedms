a448444a00489015add287a2eeffeb4f0bb4714c
vagisha <vagisha@gmail.com>
2023-10-31 16:14:21 -0700

    Custom ion names can be really long.  Increase length of the database column.

---
+++ resources/schemas/dbscripts/postgresql/targetedms-23.004-23.005.sql
@@ -0,0 +1,4 @@
+ALTER TABLE targetedms.molecule ALTER COLUMN customionname TYPE TEXT;
+ALTER TABLE targetedms.moleculeprecursor ALTER COLUMN customionname TYPE TEXT;
+ALTER TABLE targetedms.moleculetransition ALTER COLUMN customionname TYPE TEXT;
+ALTER TABLE targetedms.excludedprecursors ALTER COLUMN customionname TYPE TEXT;
