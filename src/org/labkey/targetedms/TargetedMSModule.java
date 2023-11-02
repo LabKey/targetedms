30738a722562c87873571fc1b948af8a30ba112e
vagisha <vagisha@gmail.com>
2023-11-02 08:43:50 -0700

    Custom ion names can be really long.  Increase length of the database column. (#784)

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -221,7 +221,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public Double getSchemaVersion()
     {
-        return 23.004;
+        return 23.005;
     }
 
     @Override
