03f3a539fb3823123b5ca76b353b0b87919ddcad
ankurjuneja <ankurjuneja05@gmail.com>
2024-02-02 09:30:53 -0800

    fix sql server script

--- resources/schemas/dbscripts/sqlserver/targetedms-24.000-24.001.sql
+++ resources/schemas/dbscripts/sqlserver/targetedms-24.000-24.001.sql
@@ -1,5 +1,7 @@
 ALTER TABLE targetedms.QCEnabledMetrics ADD Status NVARCHAR(20);
+GO
 UPDATE targetedms.QCEnabledMetrics SET Status = 'Disabled' WHERE Enabled = 0;
 UPDATE targetedms.QCEnabledMetrics SET Status = 'LeveyJennings' WHERE Enabled = 1;
 ALTER TABLE targetedms.QCEnabledMetrics DROP COLUMN Enabled;
+GO
 UPDATE targetedms.QCEnabledMetrics SET UpperBound = 3, LowerBound = 3 WHERE Status = 'LeveyJennings';
