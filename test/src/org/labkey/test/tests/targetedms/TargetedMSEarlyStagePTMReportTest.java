382471cd63443ef170439c669062bd86b48f62fd
labkey-jeckels <jeckels@labkey.com>
2024-02-05 14:17:15 -0800

    Test updates

--- test/src/org/labkey/test/tests/targetedms/TargetedMSEarlyStagePTMReportTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSEarlyStagePTMReportTest.java
@@ -47,20 +47,23 @@ public class TargetedMSEarlyStagePTMReportTest extends TargetedMSTest
         Assert.assertEquals("Incorrect Sample Names displayed as headers", "Sample1 Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2",
                 Locator.xpath("//table/thead[2]/tr").findElement(reportTable).getText());
 
+        int vtnRowIndex = 8;
+        int wqqRowIndex = 13;
+
         log("Verifying the modified percentage for sequence with CDR Range and stressed or not stressed updates");
-        Assert.assertEquals("Incorrect percentages for (K)VTNMDPADTATYYCAR(D) sequence", Arrays.asList("11.3%", "11.3%", "11.1%", "11.1%"),
-                reportTable.getRowDataAsText(7, "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::TotalPercentModified",
+        Assert.assertEquals("Incorrect percentages for (K)VTNMDPADTATYYCAR(D) sequence", Arrays.asList("(K)VTNMDPADTATYYCAR(D)", "11.3%", "11.3%", "11.1%", "11.1%"),
+                reportTable.getRowDataAsText(vtnRowIndex, "Sequence", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::TotalPercentModified",
                         "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2::TotalPercentModified"));
-        Assert.assertEquals("Incorrect percentages for (R)WQQGNVFSCSVMHEALHNHYTQK(S) sequence", Arrays.asList("22.1%", "22.1%", "24.1%", "24.1%"),
-                reportTable.getRowDataAsText(8, "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::TotalPercentModified",
+        Assert.assertEquals("Incorrect percentages for (R)WQQGNVFSCSVMHEALHNHYTQK(S) sequence", Arrays.asList("(R)WQQGNVFSCSVMHEALHNHYTQK(S)", "22.1%", "22.1%", "24.1%", "24.1%"),
+                reportTable.getRowDataAsText(wqqRowIndex, "Sequence", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_1::TotalPercentModified",
                         "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2::PercentModified", "Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2::TotalPercentModified"));
 
         log("Verifying the cell colors:Green, Yellow and Red");
         Assert.assertEquals("Incorrect risk category color for - Green", "rgb(137, 202, 83)",
-                Locator.xpath("//table/tbody/tr[9]/td[6]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + wqqRowIndex + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
         Assert.assertEquals("Incorrect risk category color for - Yellow", "rgb(254, 255, 63)",
-                Locator.xpath("//table/tbody/tr[8]/td[6]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + vtnRowIndex + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
         Assert.assertEquals("Incorrect risk category color for - Red", "rgb(250, 8, 26)",
-                Locator.xpath("//table/tbody/tr[8]/td[8]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + vtnRowIndex + "]/td[8]").findElement(reportTable).getCssValue("background-color"));
     }
 }
