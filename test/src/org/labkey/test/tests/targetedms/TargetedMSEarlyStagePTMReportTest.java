1865b93552392e4a792b2d70edf61c6bad62c8c1
labkey-jeckels <jeckels@labkey.com>
2024-02-05 16:11:46 -0800

    Test updates

--- test/src/org/labkey/test/tests/targetedms/TargetedMSEarlyStagePTMReportTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSEarlyStagePTMReportTest.java
@@ -47,8 +47,8 @@ public class TargetedMSEarlyStagePTMReportTest extends TargetedMSTest
         Assert.assertEquals("Incorrect Sample Names displayed as headers", "Sample1 Sc_WCL-250ng-5ngNIST_rKCTi_3hRP-20cm_DE30_QE_2",
                 Locator.xpath("//table/thead[2]/tr").findElement(reportTable).getText());
 
-        int vtnRowIndex = 8;
-        int wqqRowIndex = 13;
+        int vtnRowIndex = 1;
+        int wqqRowIndex = 7;
 
         log("Verifying the modified percentage for sequence with CDR Range and stressed or not stressed updates");
         Assert.assertEquals("Incorrect percentages for (K)VTNMDPADTATYYCAR(D) sequence", Arrays.asList("(K)VTNMDPADTATYYCAR(D)", "11.3%", "11.3%", "11.1%", "11.1%"),
@@ -60,10 +60,10 @@ public class TargetedMSEarlyStagePTMReportTest extends TargetedMSTest
 
         log("Verifying the cell colors:Green, Yellow and Red");
         Assert.assertEquals("Incorrect risk category color for - Green", "rgb(137, 202, 83)",
-                Locator.xpath("//table/tbody/tr[" + wqqRowIndex + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + (wqqRowIndex + 1) + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
         Assert.assertEquals("Incorrect risk category color for - Yellow", "rgb(254, 255, 63)",
-                Locator.xpath("//table/tbody/tr[" + vtnRowIndex + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + (vtnRowIndex + 1) + "]/td[6]").findElement(reportTable).getCssValue("background-color"));
         Assert.assertEquals("Incorrect risk category color for - Red", "rgb(250, 8, 26)",
-                Locator.xpath("//table/tbody/tr[" + vtnRowIndex + "]/td[8]").findElement(reportTable).getCssValue("background-color"));
+                Locator.xpath("//table/tbody/tr[" + (vtnRowIndex + 1) + "]/td[8]").findElement(reportTable).getCssValue("background-color"));
     }
 }
