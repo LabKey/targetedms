035fd3b8c7706608086ee95c6a28e48f72bd9c73
labkey-jeckels <jeckels@labkey.com>
2024-03-18 20:57:43 -0700

    Try to make validation of popup more robust

--- test/src/org/labkey/test/components/targetedms/SequenceCoverageWebPart.java
+++ test/src/org/labkey/test/components/targetedms/SequenceCoverageWebPart.java
@@ -77,9 +77,11 @@ public class SequenceCoverageWebPart extends BodyWebPart<SequenceCoverageWebPart
             getWrapper().shortWait().until(ExpectedConditions.elementToBeClickable(Locator.tagWithAttribute("img", "alt", "close").findElement(elementCache().peptideDetailsHelp))).click();
             getWrapper().shortWait().until(ExpectedConditions.invisibilityOf(elementCache().peptideDetailsHelp));
         }
+        Locator locator = Locator.tagWithClass("a", "_helpPopup").withText(value).childTag("div");
         WebElement numLink = getWrapper().shortWait().until(ExpectedConditions.elementToBeClickable(
-                Locator.tagWithClass("a", "_helpPopup").withText(value).childTag("div")));
-        getWrapper().mouseOver(numLink);
+                locator));
+        getWrapper().scrollIntoView(numLink);
+        getWrapper().click(locator);
         getWrapper().shortWait().until(ExpectedConditions.visibilityOf(elementCache().peptideDetailsHelp));
 
         return Locator.id("helpDivBody").withText().waitForElement(elementCache().peptideDetailsHelp, 1_000).getText();
