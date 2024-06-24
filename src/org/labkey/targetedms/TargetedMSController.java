6474a97a7e733cd17877f1e1e77976e804cf6b1a
Adam Rauch <adam@labkey.com>
2024-06-24 15:53:51 -0700

    Reference webpart name via constant (#920)

--- src/org/labkey/targetedms/TargetedMSController.java
+++ src/org/labkey/targetedms/TargetedMSController.java
@@ -7662,7 +7662,7 @@ public class TargetedMSController extends SpringActionController
                 ViewCategory category = ViewCategoryManager.getInstance().ensureViewCategory(getContainer(), getUser(), categoryParts);
                 rd.setCategoryId(category.getRowId());
 
-                ReportService.get().saveReport(getViewContext(), rd.getReportName(), report);
+                ReportService.get().saveReportEx(getViewContext(), rd.getReportName(), report);
             }
 
             ApiSimpleResponse response = new ApiSimpleResponse();
