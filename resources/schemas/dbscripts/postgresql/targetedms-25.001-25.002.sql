88f71357b595274e8881773c9e278cc479fa5d2f
ankurjuneja <ankurjuneja05@gmail.com>
2025-01-17 13:56:05 -0800

    payment method cost summary in project details view

--- resources/schemas/dbscripts/postgresql/targetedms-25.001-25.002.sql
+++ resources/schemas/dbscripts/postgresql/targetedms-25.001-25.002.sql
@@ -39,33 +39,32 @@ CREATE INDEX IDX_instrumentRate_Container ON targetedms.instrumentRate(container
 
 CREATE TABLE targetedms.instrumentUsagePayment
 (
-    Id              SERIAL NOT NULL ,
-    instrument      integer NOT NULL,
-    instrumentRate  integer NOT NULL,
-    paymentMethod   integer NOT NULL,
-    percentPayment  decimal(5,2) NOT NULL,
-    project         integer NOT NULL,
-    startTime       TIMESTAMP NOT NULL,
-    endTime         TIMESTAMP NOT NULL,
+    Id                          SERIAL NOT NULL ,
+    instrumentScheduleId        integer NOT NULL,
+    paymentMethod               integer NOT NULL,
+    percentPayment              decimal(5,2) NOT NULL,
 
-    Container       entityid NOT NULL,
-    Created         TIMESTAMP,
-    CreatedBy       USERID,
-    Modified        TIMESTAMP,
-    ModifiedBy      USERID,
+    Container                   entityid NOT NULL,
+    Created                     TIMESTAMP,
+    CreatedBy                   USERID,
+    Modified                    TIMESTAMP,
+    ModifiedBy                  USERID,
 
-    CONSTRAINT PK_instrumentUsagePayment PRIMARY KEY (Id),
-    CONSTRAINT FK_instrumentUsagePayment_instrument FOREIGN KEY (instrument) REFERENCES targetedms.msInstrument(id),
-    CONSTRAINT FK_instrumentUsagePayment_instrumentRate FOREIGN KEY (instrumentRate) REFERENCES targetedms.instrumentRate(Id),
-    CONSTRAINT FK_instrumentUsagePayment_project FOREIGN KEY (project) REFERENCES targetedms.msProject(Id)
+    CONSTRAINT PK_instrumentUsagePayment PRIMARY KEY (instrumentScheduleId, paymentMethod),
+    CONSTRAINT FK_instrumentUsagePayment_instrumentScheduleId FOREIGN KEY (instrumentScheduleId) REFERENCES targetedms.instrumentSchedule(id),
+    CONSTRAINT FK_instrumentUsagePayment_paymentMethod FOREIGN KEY (paymentMethod) REFERENCES targetedms.paymentMethod(Id)
 );
-CREATE INDEX IDX_instrumentUsagePayment_Instrument ON targetedms.instrumentUsagePayment(instrument);
-CREATE INDEX IDX_instrumentUsagePayment_InstrumentRate ON targetedms.instrumentUsagePayment(instrumentRate);
+CREATE INDEX IDX_instrumentUsagePayment_InstrumentScheduleId ON targetedms.instrumentUsagePayment(instrumentScheduleId);
 CREATE INDEX IDX_instrumentUsagePayment_PaymentMethod ON targetedms.instrumentUsagePayment(paymentMethod);
-CREATE INDEX IDX_instrumentUsagePayment_Project ON targetedms.instrumentUsagePayment(project);
 CREATE INDEX IDX_instrumentUsagePayment_Container ON targetedms.instrumentUsagePayment(container);
 
 ALTER TABLE targetedms.projectResearcher ALTER COLUMN researcher TYPE USERID;
+
+ALTER TABLE targetedms.instrumentSchedule ADD COLUMN instrumentOperator USERID;
+ALTER TABLE targetedms.instrumentSchedule ADD COLUMN instrumentRate Integer;
+ALTER TABLE targetedms.instrumentSchedule ADD CONSTRAINT FK_instrumentSchedule_instrumentRate FOREIGN KEY (instrumentRate) REFERENCES targetedms.instrumentRate(Id);
+CREATE INDEX IDX_instrumentSchedule_InstrumentOperator ON targetedms.instrumentSchedule(instrumentOperator);
+CREATE INDEX IDX_instrumentSchedule_InstrumentRate ON targetedms.instrumentSchedule(instrumentRate);
 
 
 
--- resources/schemas/dbscripts/sqlserver/targetedms-25.001-25.002.sql
+++ resources/schemas/dbscripts/sqlserver/targetedms-25.001-25.002.sql
@@ -39,14 +39,11 @@ CREATE INDEX IDX_instrumentRate_Container ON targetedms.instrumentRate(container
 
 CREATE TABLE targetedms.instrumentUsagePayment
 (
-    Id              INT IDENTITY(1, 1) NOT NULL ,
-    instrument      INT NOT NULL,
-    instrumentRate  INT NOT NULL,
-    paymentMethod   INT NOT NULL,
-    percentPayment  decimal(5,2) NOT NULL,
-    project         INT NOT NULL,
-    startTime       DATETIME NOT NULL,
-    endTime         DATETIME NOT NULL,
+    Id                          INT IDENTITY(1, 1) NOT NULL ,
+    instrumentScheduleId        INT NOT NULL,
+    instrumentRate              INT NOT NULL,
+    paymentMethod               INT NOT NULL,
+    percentPayment              decimal(5,2) NOT NULL,
 
     Container       entityid NOT NULL,
     Created         DATETIME,
@@ -55,13 +52,18 @@ CREATE TABLE targetedms.instrumentUsagePayment
     ModifiedBy      USERID,
 
     CONSTRAINT PK_instrumentUsagePayment PRIMARY KEY (Id),
-    CONSTRAINT FK_instrumentUsagePayment_instrument FOREIGN KEY (instrument) REFERENCES targetedms.msInstrument(id),
+    CONSTRAINT FK_instrumentUsagePayment_instrumentScheduleId FOREIGN KEY (instrumentScheduleId) REFERENCES targetedms.instrumentSchedule(id),
     CONSTRAINT FK_instrumentUsagePayment_instrumentRate FOREIGN KEY (instrumentRate) REFERENCES targetedms.instrumentRate(Id),
-    CONSTRAINT FK_instrumentUsagePayment_project FOREIGN KEY (project) REFERENCES targetedms.msProject(Id)
 );
-CREATE INDEX IDX_instrumentUsagePayment_Instrument ON targetedms.instrumentUsagePayment(instrument);
+CREATE INDEX IDX_instrumentUsagePayment_InstrumentScheduleId ON targetedms.instrumentUsagePayment(instrumentScheduleId);
 CREATE INDEX IDX_instrumentUsagePayment_InstrumentRate ON targetedms.instrumentUsagePayment(instrumentRate);
 CREATE INDEX IDX_instrumentUsagePayment_PaymentMethod ON targetedms.instrumentUsagePayment(paymentMethod);
-CREATE INDEX IDX_instrumentUsagePayment_Project ON targetedms.instrumentUsagePayment(project);
 CREATE INDEX IDX_instrumentUsagePayment_Container ON targetedms.instrumentUsagePayment(container);
 
+ALTER TABLE targetedms.projectResearcher ALTER COLUMN researcher TYPE USERID;
+
+ALTER TABLE targetedms.instrumentSchedule ADD COLUMN instrumentOperator USERID;
+ALTER TABLE targetedms.instrumentSchedule ADD COLUMN instrumentRate Integer;
+ALTER TABLE targetedms.instrumentSchedule ADD CONSTRAINT FK_instrumentSchedule_instrumentRate FOREIGN KEY (instrumentRate) REFERENCES targetedms.instrumentRate(Id);
+CREATE INDEX IDX_instrumentSchedule_InstrumentOperator ON targetedms.instrumentSchedule(instrumentOperator);
+CREATE INDEX IDX_instrumentSchedule_InstrumentRate ON targetedms.instrumentSchedule(instrumentRate);
