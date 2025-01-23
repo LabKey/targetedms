1fbc5752a12bbab803725bcb6cbc4bdd5c444ee1
ankurjuneja <ankurjuneja05@gmail.com>
2025-01-22 18:04:59 -0800

    fix few issues

--- resources/queries/targetedms/paymentMethodCostSummary.sql
+++ resources/queries/targetedms/paymentMethodCostSummary.sql
@@ -4,14 +4,14 @@ SELECT
     pm.name,
     pm.isCurrent,
     pm.budgetExpirationDate,
-    rt.setupFee,
-    timestampdiff('SQL_TSI_HOUR', isc.startTime, isc.endTime) * ir.fee + (SUM(iup.percentPayment * ir.fee) / 100) AS InstrumentCost,
-    timestampdiff('SQL_TSI_HOUR', isc.startTime, isc.endTime) * ir.fee + (SUM(iup.percentPayment * ir.fee) / 100) + rt.setupFee AS TotalCost
+    (SUM(iup.percentPayment * rt.setupFee) / 100) AS SetupFee,
+    timestampdiff('SQL_TSI_HOUR', isc.startTime, isc.endTime) * (SUM(iup.percentPayment * ir.fee) / 100) AS InstrumentCost,
+    timestampdiff('SQL_TSI_HOUR', isc.startTime, isc.endTime) * (SUM(iup.percentPayment * ir.fee) / 100) + (SUM(iup.percentPayment * rt.setupFee) / 100) AS TotalCost
 FROM paymentmethod pm
 INNER JOIN projectPaymentMethod ppm ON ppm.paymentMethod = pm.id
 INNER JOIN msProject p ON ppm.project = p.id
 LEFT JOIN instrumentUsagePayment iup ON iup.paymentMethod = pm.id
 LEFT JOIN instrumentSchedule isc ON iup.instrumentScheduleId = isc.id
-LEFT JOIN instrumentRate ir ON isc.instrumentRate = ir.id
+LEFT JOIN instrumentRate ir ON isc.instrument = ir.instrument
 LEFT JOIN rateType rt ON ir.rateType = rt.id
 GROUP BY p.title, pm.UWBudgetNumber, pm.name, pm.isCurrent, pm.budgetExpirationDate, rt.setupFee,ir.fee, isc.startTime, isc.endTime, p.Id
