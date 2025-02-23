42dcc021b35d40aa7ad42af8e9b2956ae4a840ab
ankurjuneja <ankurjuneja05@gmail.com>
2025-02-23 11:06:34 -0800

    code review comments

--- resources/schemas/dbscripts/sqlserver/targetedms-25.000-25.001.sql
+++ resources/schemas/dbscripts/sqlserver/targetedms-25.000-25.001.sql
@@ -4,3 +4,8 @@ ALTER TABLE targetedms.QCMetricConfiguration ADD MaxTimeValue REAL;
 GO
 ALTER TABLE targetedms.QCMetricConfiguration ADD TimeValueOption NVARCHAR(10);
 GO
+
+UPDATE targetedms.QCMetricConfiguration SET MaxTimeValue = 1000 WHERE MinTimeValue IS NOT NULL;
+GO
+UPDATE targetedms.QCMetricConfiguration SET TimeValueOption = 'First' WHERE MinTimeValue IS NOT NULL;
+GO
