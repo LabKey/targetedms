459fba9fb361ee340252ec7e963fec372222dbfd
labkey-matthewb <matthewb@labkey.com>
2025-08-02 10:57:43 -0700

    collections -> collections4

--- src/org/labkey/targetedms/TargetedMSController.java
+++ src/org/labkey/targetedms/TargetedMSController.java
@@ -27,7 +27,7 @@ import org.apache.batik.svggen.SVGGeneratorContext;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.svggen.SVGGraphics2DIOException;
 import org.apache.batik.svggen.SVGIDGenerator;
-import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
