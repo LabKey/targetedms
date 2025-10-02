364559be15a9e76d5191db19e3ee8b2f9285c96a
labkey-jeckels <jeckels@labkey.com>
2025-10-02 09:06:30 -0700

    Introduce constants and todos

--- test/src/org/labkey/test/tests/targetedms/InstrumentSchedulingTest.java
+++ test/src/org/labkey/test/tests/targetedms/InstrumentSchedulingTest.java
@@ -27,6 +27,7 @@ import org.labkey.test.util.APIContainerHelper;
 import org.labkey.test.util.ApiPermissionsHelper;
 import org.labkey.test.util.PermissionsHelper;
 import org.labkey.test.util.PortalHelper;
+import org.labkey.test.util.PostgresOnlyTest;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -36,10 +37,16 @@ import java.util.Map;
 
 @Category({})
 @BaseWebDriverTest.ClassTimeout(minutes = 5)
-public class InstrumentSchedulingTest extends TargetedMSTest
+public class InstrumentSchedulingTest extends TargetedMSTest implements PostgresOnlyTest
 {
     protected static final String SCHEDULER_USER_1 = "scheduler1@targetedms.test";
     protected static final String SCHEDULER_USER_2 = "scheduler2@targetedms.test";
+
+    public static final String INSTRUMENT_1 = "Instrument1";
+    public static final String INSTRUMENT_2 = "Instrument2";
+    public static final String INACTIVE_INSTRUMENT = "InactiveInstrument";
+    public static final String PROJECT_1 = "Project1";
+    public static final String PROJECT_2 = "Project2";
 
     @BeforeClass
     public static void initProject() throws IOException, CommandException
@@ -61,16 +68,16 @@ public class InstrumentSchedulingTest extends TargetedMSTest
 
         InsertRowsCommand instrumentInsert = new InsertRowsCommand("targetedms", "msInstrument");
         instrumentInsert.setRows(Arrays.asList(
-                Map.of("Name", "Instrument1", "Active", true, "Color", "#ee0000"),
-                Map.of("Name", "Instrument2", "Active", true, "Color", "#00ee00"),
-                Map.of("Name", "InactiveInstrument", "Active", false, "Color", "#0000ee")
+                Map.of("Name", INSTRUMENT_1, "Active", true, "Color", "#ee0000"),
+                Map.of("Name", INSTRUMENT_2, "Active", true, "Color", "#00ee00"),
+                Map.of("Name", INACTIVE_INSTRUMENT, "Active", false, "Color", "#0000ee")
         ));
         List<Map<String, Object>> instruments = instrumentInsert.execute(createDefaultConnection(), getProjectName()).getRows();
 
         InsertRowsCommand projectInsert = new InsertRowsCommand("targetedms", "msProject");
         projectInsert.setRows(Arrays.asList(
-                Map.of("Affiliation", "LabKey", "Title", "Project1", "SubmitDate", "1/1/2025", "CollaborationWith", "Mike", "ScientificQuestion", "Why do I have to enter this?", "abstract", "b"),
-                Map.of("Affiliation", "UW", "Title", "Project2", "SubmitDate", "2/2/2025", "CollaborationWith", "Josh", "ScientificQuestion", "Why is the sky blue?", "abstract", "a")
+                Map.of("Affiliation", "LabKey", "Title", PROJECT_1, "SubmitDate", "1/1/2025", "CollaborationWith", "Mike", "ScientificQuestion", "Why do I have to enter this?", "abstract", "b"),
+                Map.of("Affiliation", "UW", "Title", PROJECT_2, "SubmitDate", "2/2/2025", "CollaborationWith", "Josh", "ScientificQuestion", "Why is the sky blue?", "abstract", "a")
         ));
         List<Map<String, Object>> projects = projectInsert.execute(createDefaultConnection(), getProjectName()).getRows();
 
@@ -116,8 +123,8 @@ public class InstrumentSchedulingTest extends TargetedMSTest
     {
         goToProjectHome();
         clickAndWait(Locator.linkWithText("Your project list"));
-        assertTextPresent("Project1", "Project2");
-        clickAndWait(Locator.linkWithText("Project1"));
+        assertTextPresent(PROJECT_1, PROJECT_2);
+        clickAndWait(Locator.linkWithText(PROJECT_1));
         waitAndClickAndWait(Locator.linkWithText("Schedule instrument time"));
 
         String yearMonth = Calendar.getInstance().get(Calendar.YEAR) + "-";
@@ -136,7 +143,7 @@ public class InstrumentSchedulingTest extends TargetedMSTest
         assertProjectEventCounts(2, 0);
 
         sleep(1000);  // Wait for the dialog to clear out of the way
-        doAndWaitForPageToLoad(() -> selectOptionByText(Locator.id("projectDropDown"), "Project2"));
+        doAndWaitForPageToLoad(() -> selectOptionByText(Locator.id("projectDropDown"), PROJECT_2));
 
         scheduleInstrument(yearMonth + "-04");
         assertProjectEventCounts(1, 2);
@@ -145,24 +152,31 @@ public class InstrumentSchedulingTest extends TargetedMSTest
         assertProjectEventCounts(2, 2);
 
         sleep(1000);  // Wait for the dialog to clear out of the way
-        doAndWaitForPageToLoad(() -> selectOptionByText(Locator.id("instrumentDropDown"), "Instrument2"));
+        doAndWaitForPageToLoad(() -> selectOptionByText(Locator.id("instrumentDropDown"), INSTRUMENT_2));
         scheduleInstrument(yearMonth + "-06");
         assertProjectEventCounts(1, 0);
 
         goToDashboard();
         clickAndWait(Locator.linkWithText("All instrument calendar view"));
-        assertTextPresent("Instrument1", "Instrument2", "InactiveInstrument");
+        assertTextPresent(INSTRUMENT_1, INSTRUMENT_2, INACTIVE_INSTRUMENT);
 
         waitForElementToBeVisible(Locator.tagWithClass("div", "activeProjectEvent"));
         assertProjectEventCounts(5, 0);
 
-        selectOptionByText(Locator.id("projectFilter"), "Project2");
+        selectOptionByText(Locator.id("projectFilter"), PROJECT_2);
         assertProjectEventCounts(3, 2);
 
         goToDashboard();
         clickAndWait(Locator.linkWithText("Instrument billing report"));
         assertTextPresent("$2,450.00", 4);
         assertTextPresent("$2,690.00", 1);
+
+        // Future test cases:
+        // Split payment across multiple methods
+        // Schedule for hours within a day instead of 24-hour periods
+        // Check billing for individual months, including reservations that span month boundaries with start/end dates
+        // Ensure that overlapping reservations are rejected
+        // Ensure that reservations cannot be made for inactive instruments
     }
 
     private void assertProjectEventCounts(int expectedActiveCount, int expectedOtherCount)
