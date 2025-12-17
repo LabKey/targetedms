d88737aaa534cabb709eabd146c45d3316445a2f
Trey Chadick <tchad@labkey.com>
2025-12-17 10:17:21 -0800

    Wait longer for QC metrics page to load (#1148)

--- test/src/org/labkey/test/components/targetedms/QCSummaryWebPart.java
+++ test/src/org/labkey/test/components/targetedms/QCSummaryWebPart.java
@@ -103,7 +103,6 @@ public final class QCSummaryWebPart extends BodyWebPart<QCSummaryWebPart.Element
     public ConfigureMetricsUIPage clickConfigureQCMetrics()
     {
         clickMenuItem("Configure QC Metrics");
-        getWrapper().waitForElement(Locator.tagWithText("button", ConfigureMetricsUIPage.ADD_NEW_CUSTOM_METRIC));
         return new ConfigureMetricsUIPage(getDriver());
 
     }
