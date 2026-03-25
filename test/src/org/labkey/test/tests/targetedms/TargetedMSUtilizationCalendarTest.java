ab3d6c448a3a9e3deb7e484a6b71a218e0699fb8
ankurjuneja <ankurjuneja05@gmail.com>
2026-03-25 09:35:34 -0700

    fix test - testUtilizationCalendarActions

--- test/src/org/labkey/test/tests/targetedms/TargetedMSUtilizationCalendarTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSUtilizationCalendarTest.java
@@ -60,7 +60,7 @@ public class TargetedMSUtilizationCalendarTest extends TargetedMSTest
         log("Input data validation checks");
         utilizationCalendar.setDisplay("1")
                 .markOfflineExpectingError("2013-08-2", null, null, "Error saving. A value is required for field 'Description'")
-                .markOfflineExpectingError("2013-08-2", "2013-0802", null, "Error saving. " + getConversionErrorMessage("2013-0802", "EndDate", Timestamp.class))
+                .markOfflineExpectingError("2013-08-2", "2013-0802", null, "Error saving. Unable to convert value '2013-0802' to Date and Time")
                 .markOfflineExpectingError("2013-08-2", "2013-08-01", null, "Error saving. End date cannot be before the start date");
 
         log("Marking single day offline");
