2d82e542ace35ca1a6a9f200857047020d2ccac4
Adam Rauch <adam@labkey.com>
2026-05-01 12:52:54 -0700

    Drop redundant indices (#1199)

---
+++ resources/schemas/dbscripts/postgresql/targetedms-26.004-26.005.sql
@@ -0,0 +1,10 @@
+-- Dropping idx_instrumentusagepayment_instrumentscheduleid [instrumentScheduleId] because it overlaps with pk_instrumentusagepayment [instrumentScheduleId, paymentMethod]
+DROP INDEX targetedms.idx_instrumentusagepayment_instrumentscheduleid;
+-- Dropping ix_qcannotationtype_container [Container] because it overlaps with uq_qcannotationtype_containername [Container, Name]
+DROP INDEX targetedms.ix_qcannotationtype_container;
+-- Dropping ix_precursor_id [Id] because it overlaps with pk_precursor_id [Id]
+DROP INDEX targetedms.ix_precursor_id;
+-- Dropping ix_qcmetricexclusion_replicateid [ReplicateId] because it overlaps with uq_qcmetricexclusion_replicate_metric [ReplicateId, MetricId]
+DROP INDEX targetedms.ix_qcmetricexclusion_replicateid;
+-- Dropping ix_irtpeptide_irtscaleid [iRTScaleId] because it overlaps with uq_irtpeptide_sequenceandscale [iRTScaleId, ModifiedSequence]
+DROP INDEX targetedms.ix_irtpeptide_irtscaleid;
