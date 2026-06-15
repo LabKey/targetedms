4c77c77da5eadb556eabb5e8c1b61523915e1ec4
Josh Eckels <jeckels@labkey.com>
2026-06-15 09:51:19 -0700

    Fix PassportTest and improve scoping (#1233)
    #### Rationale
    PassportTest needs an update to expect the updated data being pulled
    dynamically from UniProt
    
    #### Changes
    * Be tolerant of changes in protein metadata from third-party service
    * Add container checks
    * Backport query name quoting

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -687,7 +687,8 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     {
         return Set.of(
             MsDataSourceUtil.TestCase.class,
-            SkylineAuditLogManager.TestCase.class
+            SkylineAuditLogManager.TestCase.class,
+            TargetedMSContainerScopingTest.class
         );
     }
 
